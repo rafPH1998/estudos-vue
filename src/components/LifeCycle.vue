15aac5b95a8673a1587935d1747b44eccfbc98f4
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-08-22 17:50:03 -0300

    first commit

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Meu nome é: {{nome}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifiCycle',
+        data() {
+          return {
+            nome: "Ainda nao sei"
+          }
+        },
+        created() {
+            setTimeout(() => {  
+                this.nome = 'Ciclanooooo'
+            }, 2000);
+        },
+        mounted() {
+            this.nome = 'Ciclano'
+        },
+    }
+
+</script>
+
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      Ola mundao!
+    </h1>
+    <p>Meu nome é: {{nome}}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+          return {
+            nome: "Rafael"
+          }
+        }
+    }
+
+</script>
