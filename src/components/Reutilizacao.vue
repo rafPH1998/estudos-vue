6a92fdd81d7d0937690645757f461331cf2f5bd6
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-18 20:24:34 -0300

    Reutilização de componentes

---
+++ src/components/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="ativa()">Clique aqui!</button>
+        <p v-show="ativado">Ativado!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Reutilizacao',
+    data() {
+        return {
+            ativado: false
+        }
+    },
+    methods: {
+        ativa() {
+            this.ativado = !this.ativado 
+        }
+    },
+}
+</script>
+
